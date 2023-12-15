c77c5c6dd6e6bc3736b003d2cb5cee62158d32f5
Scotty Jirik <jirikscotty@gmail.com>
2023-12-14 19:40:55 -0600

    I updated my css and js within my public folders

---
+++ public/js/signup.js
@@ -0,0 +1,25 @@
+const signupForm = async function(event) {
+  event.preventDefault();
+
+  const username = document.querySelector('#username-input-signup');
+  const password = document.querySelector('#password-input-signup');
+
+  const response = await fetch('/api/user', {
+    method: 'POST',
+    body: JSON.stringify({
+      username: username.value,
+      password: password.value,
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert('Failed to sign up');
+  }
+};
+
+document
+  .querySelector('#signup-form')
+  .addEventListener('submit', signupForm);
